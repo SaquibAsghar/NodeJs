e158a05ec58e8d5fe7a5636b12f2413d2d87ad8c
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-27 15:46:23 +0530

    Refactor the files

--- NodeFileSystem/WriteAFile/SyncFS/syncWriteFile.js
+++ NodeFileSystem/WriteAFile/SyncFS/syncWriteFile.js
@@ -2,13 +2,13 @@ const fs = require("fs");
 
 try {
 	fs.writeFileSync(
-		"./files/sync/notes.text",
+		"../files/sync/notes.text",
 		"This note file is created using writeFileSync method present in 'fs' module of NodeJs.\n",
 		{ encoding: "utf8", flag: "w" }
 	);
 
 	fs.appendFileSync(
-		"./files/sync/notes.text",
+		"../files/sync/notes.text",
 		"This line is appended using appendFileSync method present in 'fs' module of NodeJS.\n"
 	);
 } catch (error) {
