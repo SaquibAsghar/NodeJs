b0e65b2aa8c7fce7bc60544fd0c4a47ee4e2e85c
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-27 18:51:06 +0530

    use chalk api

--- NodeFileSystem/WriteAFile/SyncFS/syncReadFile.js
+++ NodeFileSystem/WriteAFile/SyncFS/syncReadFile.js
@@ -1,11 +1,12 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 try {
-    let r_line = fs.readFileSync('../files/sync/note.text', {encoding: 'utf8', flag:'r'})
-    console.log(r_line)
+    let r_line = fs.readFileSync('../files/sync/notes.text', {encoding: 'utf8', flag:'r'})
+    console.log(`\n${chalk.rgb(100, 250, 70)(r_line)}`)
 } catch (error) {
     if(error.errno === -4058){
-        return console.log("No such file or directory found.")
+        return console.log(chalk.red("ERROR: No such file or directory found."))
     }
-    return console.log(error)
+    return console.log(chalk.red(error))
 }
