20f70bcb8c5c2f2b570f0cc0c198726c0ad352d6
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-27 15:30:40 +0530

    Using sync file method

--- NodeFileSystem/WriteAFile/syncWriteFile.js
+++ NodeFileSystem/WriteAFile/syncWriteFile.js
@@ -2,10 +2,15 @@ const fs = require("fs");
 
 try {
 	fs.writeFileSync(
-		"notes.text",
+		'notes.text',
 		"This note file is created using writeFileSync method present in 'fs' module of NodeJs.\n",
-		{ encoding: "utf8", flag: "a" }
+		{ encoding: "utf8", flag: "w" }
+	);
+
+	fs.appendFileSync(
+		"./notes.text",
+		"This line is appended using appendFileSync method present in 'fs' module of NodeJS.\n"
 	);
 } catch (error) {
-	console.log(error);
+	console.log(error.message);
 }
