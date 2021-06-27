6adfe95998cb305c9f10b33a09e23e2e82b55761
Syed Saquib Asghar <31365464+SaquibAsghar@users.noreply.github.com>
2021-06-27 13:09:21 +0530

    Update asyncWriteFile.js

--- NodeFileSystem/WriteAFile/asyncWriteFile.js
+++ NodeFileSystem/WriteAFile/asyncWriteFile.js
@@ -1,5 +1,3 @@
 const fs = require('fs')
 
-const writeF = fs.writeFile('./test.txt', "This file is created using writeFile method present in 'fs' module of NodeJS.", ()=>{})
-
-console.log(writeF)
+fs.writeFile('./test.txt', "This file is created using writeFile method present in 'fs' module of NodeJS.", ()=>{return console.log("File write completed.")})
